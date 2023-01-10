92af7ba31e33dbaf0f2542c2145f7c1395e19f20
Loren Penny <loren_667@hotmail.com>
2023-01-10 11:25:48 +0000

    Implementing pillars and doors

--- Delve Deeper Project/Assets/Scripts/Maze.cs
+++ Delve Deeper Project/Assets/Scripts/Maze.cs
@@ -24,6 +24,7 @@ public class Maze : MonoBehaviour
         new MapLocation(-1,0),
         new MapLocation(0,-1)
     };
+    public List<MapLocation> pillarLocations = new List<MapLocation>();
 
     //values to determine the X and Z lengths of maze
     public int width = 30;
@@ -36,10 +37,12 @@ public class Maze : MonoBehaviour
     public GameObject crossroad;
     public GameObject corner;
     public GameObject tSection;
-    public GameObject deadEnd;
+    public GameObject endPiece;
     public GameObject roomWall;
     public GameObject roomFloor;
     public GameObject roomCeiling;
+    public GameObject pillar;
+    public GameObject door;
 
     public GameObject player;
 
@@ -105,24 +108,24 @@ public class Maze : MonoBehaviour
                 }
                 else if (Search2D(x, z, new int[] { 5, 1, 5, 0, 0, 1, 5, 1, 5 }))                     //horizontal dead end ->|
                 {
-                    GameObject go = Instantiate(deadEnd);
+                    GameObject go = Instantiate(endPiece);
                     go.transform.position = new Vector3(x * scale, 0, z * scale);
                     go.transform.Rotate(0, 180, 0);
                 }
                 else if (Search2D(x, z, new int[] { 5, 1, 5, 1, 0, 0, 5, 1, 5 }))                     //horizontal dead end |<-
                 {
-                    GameObject go = Instantiate(deadEnd);
+                    GameObject go = Instantiate(endPiece);
                     go.transform.position = new Vector3(x * scale, 0, z * scale);
                 }
                 else if (Search2D(x, z, new int[] { 5, 1, 5, 1, 0, 1, 5, 0, 5 }))                     //vertical dead end T
                 {
-                    GameObject go = Instantiate(deadEnd);
+                    GameObject go = Instantiate(endPiece);
                     go.transform.position = new Vector3(x * scale, 0, z * scale);
                     go.transform.Rotate(0, 90, 0);
                 }
                 else if (Search2D(x, z, new int[] { 5, 0, 5, 1, 0, 1, 5, 1, 5 }))                     //vertical dead end downT
                 {
-                    GameObject go = Instantiate(deadEnd);
+                    GameObject go = Instantiate(endPiece);
                     go.transform.position = new Vector3(x * scale, 0, z * scale);
                     go.transform.Rotate(0, -90, 0);
                 }
@@ -192,66 +195,175 @@ public class Maze : MonoBehaviour
                                             CountSquareNeighbours(x,z) >= 1 && CountDiagonalNeighbours(x,z) > 1))
                 {
                     GameObject floor = Instantiate(roomFloor);
-                    floor.transform.position = new Vector3(x * scale, -3f, z * scale);
+                    floor.transform.position = new Vector3(x * scale, 0, z * scale);
 
                     GameObject ceiling = Instantiate(roomCeiling);
                     ceiling.transform.position = new Vector3(x * scale, 0, z * scale);
 
+                    GameObject cornerPillar;
                     LocateWalls(x, z);
-                    if (topWall)
+                    if (north)
                     {
                         GameObject wall1 = Instantiate(roomWall);
                         wall1.transform.position = new Vector3(x * scale, 0, z * scale);
                         wall1.transform.Rotate(0, 90, 0);
+                        wall1.name = "North Wall";
+
+                        if (map[x + 1, z] == 0 && map[x + 1, z + 1] == 0 && !pillarLocations.Contains(new MapLocation(x,z)))
+                        {
+                            cornerPillar = Instantiate(pillar);
+                            cornerPillar.transform.position = new Vector3((x + .5f) * scale, 0, (z + .5f) * scale);
+                            cornerPillar.name = "North Right";
+                            pillarLocations.Add(new MapLocation(x, z));
+                        }
+
+                        if (map[x - 1, z] == 0 && map[x - 1, z + 1] == 0 && !pillarLocations.Contains(new MapLocation(x - 1, z)))
+                        {
+                            cornerPillar = Instantiate(pillar);
+                            cornerPillar.transform.position = new Vector3((x - .5f) * scale, 0, (z + .5f) * scale);
+                            cornerPillar.name = "North Left";
+                            pillarLocations.Add(new MapLocation(x - 1, z));
+                        }
                     }
 
-                    if (bottomWall)
+                    if (south)
                     {
                         GameObject wall2 = Instantiate(roomWall);
                         wall2.transform.position = new Vector3(x * scale, 0, z * scale);
                         wall2.transform.Rotate(0, -90, 0);
+                        wall2.name = "South Wall";
+
+                        if (map[x + 1, z] == 0 && map[x + 1, z - 1] == 0 && !pillarLocations.Contains(new MapLocation(x, z - 1)))
+                        {
+                            cornerPillar = Instantiate(pillar);
+                            cornerPillar.transform.position = new Vector3((x + .5f) * scale, 0, (z - .5f) * scale);
+                            cornerPillar.name = "South Right";
+                            pillarLocations.Add(new MapLocation(x, z - 1));
+                        }
+
+                        if (map[x - 1, z - 1] == 0 && map[x - 1, z] == 0 && !pillarLocations.Contains(new MapLocation(x - 1, z - 1)))
+                        {
+                            cornerPillar = Instantiate(pillar);
+                            cornerPillar.transform.position = new Vector3((x - .5f) * scale, 0, (z - .5f) * scale);
+                            cornerPillar.name = "South Left";
+                            pillarLocations.Add(new MapLocation(x - 1, z - 1));
+                        }
                     }
 
-                    if (rightWall)
+                    if (east)
                     {
                         GameObject wall3 = Instantiate(roomWall);
                         wall3.transform.position = new Vector3(x * scale, 0, z * scale);
                         wall3.transform.Rotate(0, 180, 0);
+                        wall3.name = "East Wall";
+
+                        if (map[x + 1, z + 1] == 0 && map[x, z + 1] == 0 && !pillarLocations.Contains(new MapLocation(x, z - 1)))
+                        {
+                            cornerPillar = Instantiate(pillar);
+                            cornerPillar.transform.position = new Vector3((x + .5f) * scale, 0, (z + .5f) * scale);
+                            cornerPillar.name = "East Top";
+                            pillarLocations.Add(new MapLocation(x, z - 1));
+                        }
+
+                        if (map[x, z - 1] == 0 && map[x + 1, z - 1] == 0 && !pillarLocations.Contains(new MapLocation(x + 1, z - 1)))
+                        {
+                            cornerPillar = Instantiate(pillar);
+                            cornerPillar.transform.position = new Vector3((x + .5f) * scale, 0, (z - .5f) * scale);
+                            cornerPillar.name = "East Bottom";
+                            pillarLocations.Add(new MapLocation(x + 1, z - 1));
+                        }
                     }
 
-                    if (leftWall)
+                    if (west)
                     {
                         GameObject wall4 = Instantiate(roomWall);
                         wall4.transform.position = new Vector3(x * scale, 0, z * scale);
+                        wall4.name = "West Wall";
+
+                        if (map[x - 1, z + 1] == 0 && map[x, z + 1] == 0 && !pillarLocations.Contains(new MapLocation(x - 1, z)))
+                        {
+                            cornerPillar = Instantiate(pillar);
+                            cornerPillar.transform.position = new Vector3((x - .5f) * scale, 0, (z + .5f) * scale);
+                            cornerPillar.name = "West Top";
+                            pillarLocations.Add(new MapLocation(x - 1, z));
+                        }
+
+                        if (map[x - 1, z - 1] == 0 && map[x, z - 1] == 0 && !pillarLocations.Contains(new MapLocation(x - 1, z - 1)))
+                        {
+                            cornerPillar = Instantiate(pillar);
+                            cornerPillar.transform.position = new Vector3((x - .5f) * scale, 0, (z - .5f) * scale);
+                            cornerPillar.name = "West Bottom";
+                            pillarLocations.Add(new MapLocation(x - 1, z - 1));
+                        }
                     }
-                }
-                else
-                {
-                    Vector3 pos = new Vector3(x * scale, 0, z * scale);
-                    GameObject roomTile = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    roomTile.transform.localScale = new Vector3(scale, scale, scale);
-                    roomTile.transform.position = pos;
+
+                    /*GameObject doorway;
+                    LocateDoors(x, z);
+                    if (north)
+                    {
+                        doorway = Instantiate(door);
+                        doorway.transform.position = new Vector3(x * scale, 0, z * scale);
+                        doorway.transform.Rotate(0, 180, 0);
+                        doorway.name = "North Door";
+                    }
+
+                    if (south)
+                    {
+                        doorway = Instantiate(door);
+                        doorway.transform.position = new Vector3(x * scale, 0, z * scale);
+                        doorway.name = "South Door";
+                    }
+
+                    if (east)
+                    {
+                        doorway = Instantiate(door);
+                        doorway.transform.position = new Vector3(x * scale, 0, z * scale);
+                        doorway.transform.Rotate(0, 90, 0);
+                        doorway.name = "East Door";
+                    }
+
+                    if (west)
+                    {
+                        doorway = Instantiate(door);
+                        doorway.transform.position = new Vector3(x * scale, 0, z * scale);
+                        doorway.transform.Rotate(0, -90, 0);
+                        doorway.name = "West Door";
+                    }*/
                 }
             }
     }
 
-    bool topWall;
-    bool bottomWall;
-    bool rightWall;
-    bool leftWall;
+    bool north;
+    bool south;
+    bool east;
+    bool west;
 
     public void LocateWalls(int x, int z)
     {
-        topWall = false;
-        bottomWall = false;
-        rightWall = false;
-        leftWall = false;
+        north = false;
+        south = false;
+        east = false;
+        west = false;
 
         if (x <= 0 || x >= width - 1 || z <= 0 || z >= depth - 1) return;
-        if (map[x, z + 1] == 1) topWall = true;
-        if (map[x, z - 1] == 1) bottomWall = true;
-        if (map[x + 1, z] == 1) rightWall = true;
-        if (map[x - 1, z] == 1) leftWall = true;
+        if (map[x, z + 1] == 1) north = true;
+        if (map[x, z - 1] == 1) south = true;
+        if (map[x + 1, z] == 1) east = true;
+        if (map[x - 1, z] == 1) west = true;
+    }
+
+    public void LocateDoors(int x, int z)
+    {
+        north = false;
+        south = false;
+        east = false;
+        west = false;
+
+        if (x <= 0 || x >= width - 1 || z <= 0 || z >= depth - 1) return;
+        if (map[x, z + 1] == 0 && map[x - 1, z + 1] == 1 && map[x + 1, z + 1] == 1) north = true;
+        if (map[x, z - 1] == 0 && map[x - 1, z - 1] == 1 && map[x + 1, z - 1] == 1) south = true;
+        if (map[x + 1, z] == 0 && map[x + 1, z + 1] == 1 && map[x + 1, z - 1] == 1) east = true;
+        if (map[x - 1, z] == 0 && map[x - 1, z + 1] == 1 && map[x - 1, z - 1] == 1) west = true;
     }
 
     public virtual void PlacePlayerInMaze()
@@ -261,7 +373,7 @@ public class Maze : MonoBehaviour
             {
                 if (map[x, z] == 0)
                 {
-                    player.transform.position = new Vector3(x * scale, -2.75f, z * scale);
+                    player.transform.position = new Vector3(x * scale, 0, z * scale);
                     return;
                 }
             }
