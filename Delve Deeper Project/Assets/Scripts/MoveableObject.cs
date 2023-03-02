2cb4911eb89ae3b919a27b4c9ecddca782cb4766
Loren Penny <loren_667@hotmail.com>
2023-03-02 10:39:11 +0000

    Configured lighting settings

---
+++ Delve Deeper Project/Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(AudioSource))]
+public class MoveableObject : MonoBehaviour, IInteractable
+{
+    Rigidbody rb;
+    AudioSource audioSource;
+    [SerializeField] private string interactText;
+    [SerializeField] private AudioClip movingSound;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = movingSound;
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity.magnitude >= 0.1 && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void Interact(Transform interactorTransform)
+    {
+        Debug.Log("Statue");
+    }
+
+    public string GetInteractText()
+    {
+        return interactText;
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+}
